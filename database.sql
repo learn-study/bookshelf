1b2298904d81c725e251364da0c20bd39c09c570
hogehoge <ohayokki@yahoo.co.jp>
2018-05-14 02:28:20 +0000

    first

---
+++ database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE bookshelf;
+CREATE TABLE bookshelf.books (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(100),
+    image_url VARCHAR(100),
+    status VARCHAR(10),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
